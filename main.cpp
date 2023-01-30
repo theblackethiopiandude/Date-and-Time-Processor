7ef08b9e47fa3a105720f18403a34d09262378da
eyosi <eyosi.iy9@gmail.com>
2023-01-31 02:21:49 +0300

    corrected GC Leap year selection

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main() {
                 cout << "Enter year in GC: ";
                 cin >> year;
                 if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0)
-                    cout << year << " is not a leap year" << endl;
+                    cout << year << " is a leap year" << endl;
                 else
                     cout << year << " is not a leap year" << endl;
 
